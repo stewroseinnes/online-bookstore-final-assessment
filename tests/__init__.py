af145c90af8769f835c4bd29492891fc5734740c
stewroseinnes <61104800+stewroseinnes@users.noreply.github.com>
2025-10-19 19:07:34 +0100

    Create __init__.py in tests directory
    Add init file to tests directory for package recognition

---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# This file can be empty.
+# Its presence tells Python that the 'tests' directory should be treated as a package,
+# which helps pytest discover the test files and modules within it correctly.
